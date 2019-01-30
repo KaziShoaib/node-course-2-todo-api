ff03ffc180f0cf796227c8ed3c949e06a572f926
Kazi Shoaib Muhammad <shoaib.iiuc.cse@gmail.com>
2019-01-30 16:48:02 +0600

    Correct mongoose connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,7 +5,7 @@ let localDB = 'mongodb://localhost:27017/TodoApp';
 //let cloudDB = "mongodb://heroku-node-todo-api:heroku123@ds037283.mlab.com:37283/todo_apps_db?authSource=todo_apps_db&w=1";
 mongoose.connect(process.env.MONGODB_URI||process.env.MONGOLAB_URI||localDB);
 //mongoose.connect(localDB);
-mongoose.connect(localDB,  {useNewUrlParser: true});
+//mongoose.connect(localDB,  {useNewUrlParser: true});
 //console.log(process.env.MONGOLAB_URI);
 //console.log(process.env.MONGODB_URI);
 module.exports = {mongoose};
