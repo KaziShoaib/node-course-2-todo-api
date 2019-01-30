6b0ae429406631adf09fee3c929bc1ea32c7fd39
Kazi Shoaib Muhammad <shoaib.iiuc.cse@gmail.com>
2019-01-30 13:52:46 +0600

    Set up app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+let localDB = 'mongodb://localhost:27017/TodoApp';
+let cloudDB = 'mongodb://KaziShoaib:_q91kt19q@ds037283.mlab.com:37283/todo_apps_db'
+mongoose.connect(cloudDB);
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ let {Todo} = require('./models/todo.js');
 let {User} = require('./models/user.js');
 
 let app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -42,8 +43,8 @@ app.get('/todos/:id',(req, res)=>{
 	});
 });
 
-app.listen(3000, ()=>{
-  console.log('Started on port 3000');
+app.listen(port, ()=>{
+  console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
