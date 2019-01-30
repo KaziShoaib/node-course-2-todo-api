42d937b5d4959ffa05c2040b3db503cbb1e61c45
Kazi Shoaib Muhammad <shoaib.iiuc.cse@gmail.com>
2019-01-30 14:55:53 +0600

    ADD mongolab URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,10 @@
 let mongoose = require('mongoose');
-
 mongoose.Promise = global.Promise;
 let localDB = 'mongodb://localhost:27017/TodoApp';
-let cloudDB = 'mongodb://KaziShoaib:_q91kt19q@ds037283.mlab.com:37283/todo_apps_db'
-mongoose.connect(cloudDB);
+let cloudDB = 'mongodb://heroku-node-todo-api:heroku123@ds037283.mlab.com:37283/todo_apps_db'
 
+mongoose.connect(process.env.MONGODB_URI||process.env.MONGOLAB_URI||localDB);
+//mongoose.connect(cloudDB);
+//console.log(process.env.MONGOLAB_URI);
+//console.log(process.env.MONGODB_URI);
 module.exports = {mongoose};
