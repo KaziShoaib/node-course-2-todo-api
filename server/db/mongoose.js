ac2770b741dafe1e8e754e5156be5e57098d037a
Kazi Shoaib Muhammad <shoaib.iiuc.cse@gmail.com>
2019-01-30 16:38:51 +0600

    Update mongodb and mongoose

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,11 @@
 let mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 let localDB = 'mongodb://localhost:27017/TodoApp';
-let cloudDB = 'mongodb://heroku-node-todo-api:heroku123@ds037283.mlab.com:37283/todo_apps_db'
-
+//let cloudDB = "mongodb://hello:hello123@ds037283.mlab.com:37283/todo_apps_db";
+//let cloudDB = "mongodb://heroku-node-todo-api:heroku123@ds037283.mlab.com:37283/todo_apps_db?authSource=todo_apps_db&w=1";
 mongoose.connect(process.env.MONGODB_URI||process.env.MONGOLAB_URI||localDB);
-//mongoose.connect(cloudDB);
+//mongoose.connect(localDB);
+mongoose.connect(localDB,  {useNewUrlParser: true});
 //console.log(process.env.MONGOLAB_URI);
 //console.log(process.env.MONGODB_URI);
 module.exports = {mongoose};
